ba778fbc4c8d8e996032ccf83b19a051519728a7
gusta0912 <69582471+gusta0912@users.noreply.github.com>
2021-05-21 09:27:23 -0300

    bd

--- trabalho2b (3).sql
+++ trabalho2b (3).sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Tempo de geração: 20-Maio-2021 às 15:41
+-- Tempo de geração: 21-Maio-2021 às 14:26
 -- Versão do servidor: 10.4.11-MariaDB
 -- versão do PHP: 7.4.3
 
@@ -55,6 +55,26 @@ INSERT INTO `cadastrar` (`id`, `emailCliente`, `senhaCliente`, `image`) VALUES
 -- --------------------------------------------------------
 
 --
+-- Estrutura da tabela `city`
+--
+
+CREATE TABLE `city` (
+  `idCity` int(11) NOT NULL,
+  `nameCity` varchar(300) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Extraindo dados da tabela `city`
+--
+
+INSERT INTO `city` (`idCity`, `nameCity`) VALUES
+(1, 'sumare'),
+(2, 'londrina'),
+(22, 'Campinas');
+
+-- --------------------------------------------------------
+
+--
 -- Estrutura da tabela `consultas`
 --
 
@@ -63,6 +83,13 @@ CREATE TABLE `consultas` (
   `Nome_paciente` varchar(40) NOT NULL,
   `Valor_consulta` varchar(60) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Extraindo dados da tabela `consultas`
+--
+
+INSERT INTO `consultas` (`ID_consulta`, `Nome_paciente`, `Valor_consulta`) VALUES
+(1, 'Joao', '140');
 
 -- --------------------------------------------------------
 
@@ -74,6 +101,21 @@ CREATE TABLE `exame` (
   `ID_exame` smallint(6) NOT NULL,
   `Nome_paciente` varchar(40) NOT NULL,
   `Resultado_exame` varchar(60) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+-- Erro ao ler dados para tabela trabalho2b.exame: #1064 - Você tem um erro de sintaxe no seu SQL próximo a 'FROM `trabalho2b`.`exame`' na linha 1
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `logging`
+--
+
+CREATE TABLE `logging` (
+  `idLogging` int(11) NOT NULL,
+  `dateLogging` datetime NOT NULL,
+  `level` varchar(100) NOT NULL,
+  `msg` varchar(1000) NOT NULL,
+  `fk_reg` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 -- --------------------------------------------------------
@@ -88,6 +130,13 @@ CREATE TABLE `medico` (
   `Sobrenome_medico` varchar(60) DEFAULT NULL,
   `ID_consulta` int(11) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Extraindo dados da tabela `medico`
+--
+
+INSERT INTO `medico` (`ID_medico`, `Nome_medico`, `Sobrenome_medico`, `ID_consulta`) VALUES
+(1, 'Fernando', 'Castanhare', 1);
 
 -- --------------------------------------------------------
 
@@ -135,6 +184,18 @@ INSERT INTO `produtos` (`id`, `nome`, `quantidade`, `preco`) VALUES
 -- --------------------------------------------------------
 
 --
+-- Estrutura da tabela `profile_reg`
+--
+
+CREATE TABLE `profile_reg` (
+  `idProfile` int(11) NOT NULL,
+  `nameProfile` varchar(500) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+-- Erro ao ler dados para tabela trabalho2b.profile_reg: #1064 - Você tem um erro de sintaxe no seu SQL próximo a 'FROM `trabalho2b`.`profile_reg`' na linha 1
+
+-- --------------------------------------------------------
+
+--
 -- Estrutura da tabela `profissional`
 --
 
@@ -156,33 +217,17 @@ CREATE TABLE `reg` (
   `password` varchar(50) NOT NULL,
   `city` varchar(15) NOT NULL,
   `image` varchar(50) NOT NULL,
-  `gender` varchar(10) NOT NULL
+  `gender` varchar(10) NOT NULL,
+  `id` int(11) NOT NULL,
+  `fk_idProfile` int(11) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Extraindo dados da tabela `reg`
 --
 
-INSERT INTO `reg` (`name`, `username`, `password`, `city`, `image`, `gender`) VALUES
-('gustavo', 'gomes', '123', '', 'image/imagem.jpg', 'male'),
-('gustavo', 'gomes', '123', '', 'image/imagem.jpg', 'male'),
-('gustavo', 'gomes', '123', '', 'image/imagem.jpg', 'male'),
-('gustavo', 'gomes', '23123', '', 'image/i.png', 'male'),
-('gustavo', 'gomes', 'qwwrqw13', '', 'image/i2.jpeg', 'male'),
-('gustavo', 'gomes', 'qwwrqw13', '', 'image/i2.jpeg', 'male'),
-('gustavo', 'gomes', '1232', '', 'image/imagem.jpg', 'male'),
-('gustavo', 'gustavo123@gmail.com', '12345', '', '', ''),
-('gustavo', 'gustavo123@gmail.com', '12345', '', '', ''),
-('gustavo', 'gustavo123@gmail.com', '121321', '', '', ''),
-('gustavo', 'gustavo123@gmail.com', '121321', '', '', ''),
-('gustavo', 'gustavo123@gmail.com', '121321', '', '', ''),
-('gustavo', 'gustavo123@gmail.com', '121321', '', '', ''),
-('gustavo', 'gustavo123@gmail.com', '121321', '', '', ''),
-('gustavo', 'gomes', '1213', '', 'image/i2.jpeg', 'male'),
-('gustavo', 'gomes', '12321', '', '', 'female'),
-('gustavo', 'gomes', '12321', '', '', 'female'),
-('gustavo', '2q321', '2q31231212312', '', '', 'male'),
-('gustavo', '2q321', '2q31231212312', '', '', 'male');
+INSERT INTO `reg` (`name`, `username`, `password`, `city`, `image`, `gender`, `id`, `fk_idProfile`) VALUES
+('gustavo', 'gu', '123', 'londrina', '', 'male', 1, 1);
 
 --
 -- Índices para tabelas despejadas
@@ -195,6 +240,12 @@ ALTER TABLE `cadastrar`
   ADD PRIMARY KEY (`id`);
 
 --
+-- Índices para tabela `city`
+--
+ALTER TABLE `city`
+  ADD PRIMARY KEY (`idCity`);
+
+--
 -- Índices para tabela `consultas`
 --
 ALTER TABLE `consultas`
@@ -205,6 +256,13 @@ ALTER TABLE `consultas`
 --
 ALTER TABLE `exame`
   ADD PRIMARY KEY (`ID_exame`);
+
+--
+-- Índices para tabela `logging`
+--
+ALTER TABLE `logging`
+  ADD PRIMARY KEY (`idLogging`),
+  ADD KEY `fk_reg` (`fk_reg`);
 
 --
 -- Índices para tabela `medico`
@@ -225,10 +283,23 @@ ALTER TABLE `produtos`
   ADD PRIMARY KEY (`id`);
 
 --
+-- Índices para tabela `profile_reg`
+--
+ALTER TABLE `profile_reg`
+  ADD PRIMARY KEY (`idProfile`);
+
+--
 -- Índices para tabela `profissional`
 --
 ALTER TABLE `profissional`
   ADD PRIMARY KEY (`ID_profissional`);
+
+--
+-- Índices para tabela `reg`
+--
+ALTER TABLE `reg`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `fk_idProfile` (`fk_idProfile`);
 
 --
 -- AUTO_INCREMENT de tabelas despejadas
@@ -239,6 +310,18 @@ ALTER TABLE `profissional`
 --
 ALTER TABLE `cadastrar`
   MODIFY `id` int(13) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
+
+--
+-- AUTO_INCREMENT de tabela `city`
+--
+ALTER TABLE `city`
+  MODIFY `idCity` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=23;
+
+--
+-- AUTO_INCREMENT de tabela `logging`
+--
+ALTER TABLE `logging`
+  MODIFY `idLogging` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
 
 --
 -- AUTO_INCREMENT de tabela `pedidos`
@@ -253,8 +336,26 @@ ALTER TABLE `produtos`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
+-- AUTO_INCREMENT de tabela `profile_reg`
+--
+ALTER TABLE `profile_reg`
+  MODIFY `idProfile` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT de tabela `reg`
+--
+ALTER TABLE `reg`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=29;
+
+--
 -- Restrições para despejos de tabelas
 --
+
+--
+-- Limitadores para a tabela `logging`
+--
+ALTER TABLE `logging`
+  ADD CONSTRAINT `logging_ibfk_1` FOREIGN KEY (`fk_reg`) REFERENCES `reg` (`id`);
 
 --
 -- Limitadores para a tabela `medico`
