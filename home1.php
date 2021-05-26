3072ce3ab28e9cbfb053069c02ec926ae4065389
gusta0912 <69582471+gusta0912@users.noreply.github.com>
2021-05-26 10:54:45 -0300

    finalizando trabalho

--- home1.php
+++ home1.php
@@ -21,11 +21,8 @@ body {font-family: "Lato", sans-serif}
     <a href="home.php" class="w3-bar-item w3-button w3-padding-large w3-hide-small">SEU PERFIL</a>
     <a href="#contact" class="w3-bar-item w3-button w3-padding-large w3-hide-small">CONTACT</a>
     <div class="w3-dropdown-hover w3-hide-small">
-      <button class="w3-padding-large w3-button" title="More">VER MAIS <i class="fa fa-caret-down"></i></button>     
-      <div class="w3-dropdown-content w3-bar-block w3-card-4">
-        <a href="#" class="w3-bar-item w3-button">ver medicos disponiveis</a>
-        <a href="#" class="w3-bar-item w3-button"></a>
-        <a href="#" class="w3-bar-item w3-button">Media</a>
+      
+     
       </div>
     </div>
     <a href="javascript:void(0)" class="w3-padding-large w3-hover-red w3-hide-small w3-right"><i class="fa fa-search"></i></a>
